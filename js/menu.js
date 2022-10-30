d244c01369b5dd84411dab1bfd1b2c199844d185
Tim Sundell <tim920129@hotmail.com>
2022-10-30 18:41:44 +0100

    :sparkles: Added JS for hamburgermenu toogle.

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+//Simple script to open/close the hamburger menu vertically
+function openMenu() {
+  document.getElementById("myMenu").style.width = "100%";
+}
+
+function closeMenu() {
+  document.getElementById("myMenu").style.width = "0";
+}
